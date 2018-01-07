2c2532955196af6d758de76b68ebba6515fad3ba
ingielek <ingielewicz.p@gmail.com>
2018-01-07 20:42:08 +0100

    next commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //https://teamtreehouse.com/paweingielewicz.json
 const https = require('https');
+const http = require('http');
+function printError(error) {
+   console.error(error.message);
+}
 function printMessage (username, badgeCount, points) {
    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in Javascript`;
    console.log(message);
@@ -7,22 +11,31 @@ function printMessage (username, badgeCount, points) {
 function getProfile(username) {
    try {
       const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-       let body = "";
-       response.on('data', data => {
-           body += data.toString();
-       });
+         if(response.statusCode === 200){
+             let body = "";
+             response.on('data', data => {
+                 body += data.toString();
+             });
 
-       response.on('end', () => {
-           const profile = JSON.parse(body);
-           printMessage(username, profile.badges.length, profile.points.JavaScript);
-       });
+             response.on('end', () => {
+                 try{const profile = JSON.parse(body);
+                     printMessage(username, profile.badges.length, profile.points.JavaScript);
+                 } catch (error){
+                     printError(error)
+                 }
+             });
+         } else {
+            const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+         }
    });
-       request.on('error', error => console.error(`Problem with request: ${error.message}`))
+       request.on('error', error => printError(error))
    }
    catch (error) {
-      console.error(error.message);
+       printError(error)
    }
 }
 
-const users = process.argv.slice(2)
+const users = process.argv.slice(2);
 users.forEach(getProfile);
